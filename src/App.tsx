6904cd25e3f06145c52462bb96f64a2a1fa9899b
Isaac Aguiar <37755163+AguiarIsaac@users.noreply.github.com>
2022-07-14 18:12:33 +0000

    tentativa de logica de task

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import './Global.module.css'
 import styles from './App.module.css'
 import Plus from './Assets/plus.svg'
 import { Header } from './Components/Header'
-import { TaskEmpty } from './Components/TaskEmpty'
 import { TaskList } from './Components/TaskList'
-
+import { ChangeEvent, FormEvent, useState } from 'react'
 
 
 export function App() {
-  const task = [
+
+  const [task, setTask] = useState([
     {
       id: '1',
       taskContent: 'Terminar estilização da aplicação.',
@@ -20,23 +20,41 @@ export function App() {
     {
       id: '3',
       taskContent: 'Fazer deploy.',
+    },
+    {
+      id: '4',
+      taskContent: 'Usar o stato e deixar a incerção de dados dinamica',
     }
-  ]
-  
+  ])
+
+  function handleNewTask(event: FormEvent) {
+    event.target.setCustomValidity('')
+    
+    const insertNewTask = {
+      id: 'task.length + 1',
+      taskContent: event.target.value
+    }
+
+    setTask([...task, insertNewTask])
+
+  }
+
   return (
     <>
       <Header />
       <div className={styles.wrapper}>
         <div className={styles.newTask}>
-          <form>
-              <textarea placeholder="Adicione uma nova tarefa"/>
+          <form onSubmit={handleNewTask}>
+              <textarea
+                placeholder="Adicione uma nova tarefa"
+              />
               <button type="submit">Criar <img src={Plus} alt="Icone de adição"/></button>
           </form>
         </div>
 
         <div className={styles.tasksInfo}>
           <p>Tarefas criadas <span>{task.length}</span></p>
-          <p>Concluídas <span>0</span></p>
+          <p>Concluídas <span>0 de 0</span></p>
         </div>
 
         {
